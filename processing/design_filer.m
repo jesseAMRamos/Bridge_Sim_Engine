1363a60f850d324d290fd1f93e419ed1d23145a3
dannyrha27 <danieljunrha@gmail.com>
2026-04-05 17:28:53 -0400

    added chat code

--- processing/design_filer.m
+++ processing/design_filer.m
@@ -1,3 +1,44 @@
 % Will be master command to bring computations together
 % layer to interacts directly with files
 % generate the design_file.mat that will be submitted
+    loaded = load('dim_table.mat');
+
+    tableData = loaded.tableData;
+    columnNames = loaded.columnNames;
+
+    % Strip empty rows
+    mask = ~cellfun(@isempty, tableData(:,1));
+    tableData = tableData(mask, :);
+    n = size(tableData,1);
+    joints    = zeros(n, 2);
+    memberEnds = zeros(n, 2);
+
+    %convert from strings to doubles
+    for i = 1:length(tableData)
+        joints(i,:) = [str2double(tableData{i,1}),str2double(tableData{i,2})];
+        memberEnds(i,:) = [str2double(tableData{i,3}),str2double(tableData{i,4})];
+    end
+    
+    %create mapping array to assign indexes for all joints
+    uniqueJoints = [];
+    coords = tableData(:, 1:2);       % extract coordinates
+    %coords = cell2mat(coords);        % convert cell to numeric array if needed
+
+    jointMap = zeros(n,1);            % array to store the index mapping
+    index = 0;                         % counter for unique joints
+    for i = 1:n
+    % Check if current coordinate is already in uniqueJoints
+    [isFound, loc] = ismember(coords(i,:), uniqueJoints, 'rows');
+        if isFound
+            jointMap(i) = loc;        % use existing index
+        else
+            index = index + 1;
+            uniqueJoints(index,:) = coords(i,:);  % add new unique joint
+            jointMap(i) = index;
+        end
+    end
+
+
+
+    joints
+    jointMap
--- processing/dimToVector.m
+++ processing/dimToVector.m
@@ -2,9 +2,19 @@
 %position matix C, as shown in the lab manual
 % (Normalized mean more of the form of matrix A -> [Cx, Cy]T)
 
-function [C,Sx,Sy,L] = dimToComponent(startJoints, endJoints, uniqueJoints)
+function [C,Sx,Sy,L] = dimToComponent(startJoints, endJoints, uniqueJoints, jointMap)
+    C = [];
+    
+    for i = 1:length(startJoints)
+        end_start = (endJoints(i) - start_end(i)) / sqrt(startJoints(i)^2 + endJoints(i)^2);
+        start_end = (startJoints(i) - endJoints(i)) / sqrt(startJoints(i)^2 + endJoints(i)^2);
+        C(i,jointMap(i)) = end_start;
+        C(i,jointMap())
+        
+        
 
-end 
+    end 
+end
 
 % Use the helper function above to construct Matrix A
 function [A] = dimToVector(startJoints, endJoints, uniqueJoints)
