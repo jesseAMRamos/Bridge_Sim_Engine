ef0e45e0e496d9fab8acac3f945bf6def5981443
Jesse Madrid Ramos <jesse637@bu.edu>
2026-04-08 15:56:00 -0400

    migrated to app designer and added force gauges

--- processing/design_filer.m
+++ processing/design_filer.m
@@ -2,91 +2,94 @@
 % layer to interacts directly with files
 % generate the design_file.mat that will be submitted
 
-loaded = load('dim_table.mat');
-tableData = loaded.tableData;
-columnNames = loaded.columnNames;
-% Strip empty rows
-mask = ~cellfun(@isempty, tableData(:,1));
-tableData = tableData(mask, :);
-n = size(tableData,1);
-joints    = zeros(n, 2);
-memberEnds = zeros(n, 2);
-%convert from strings to doubles
-for i = 1:length(tableData)
-    joints(i,:) = [str2double(tableData{i,1}),str2double(tableData{i,2})];
-    memberEnds(i,:) = [str2double(tableData{i,3}),str2double(tableData{i,4})];
-end
-
-[uniqueJoints,~,jointMap] = unique([joints;memberEnds], 'rows', 'stable');
-%jointMap
-%jointMap
-%example on how to index map
-%{
-    for i = 1:size(joints(:,1))
-    disp(joints(i,:))
-    disp(uniqueJoints(jointMap(i),:))
-end  
-%}
-
-[A,names,r] = dimToVector(joints,memberEnds,uniqueJoints);
-index = 0;
-big = uniqueJoints(1,1);
-for i = 2:size(uniqueJoints,1)
-    if uniqueJoints(i,1) >= big
-        big = uniqueJoints(i,1);
-        index = i;
+function [Tension,names] = design_filer(F,jointIndex)
+    loaded = load('dim_table.mat');
+    tableData = loaded.tableData;
+    %columnNames = loaded.columnNames;
+    % Strip empty rows
+    mask = ~cellfun(@isempty, tableData(:,1));
+    tableData = tableData(mask, :);
+    n = size(tableData,1);
+    joints    = zeros(n, 2);
+    memberEnds = zeros(n, 2);
+    %convert from strings to doubles
+    for i = 1:length(tableData)
+        joints(i,:) = [str2double(tableData{i,1}),str2double(tableData{i,2})];
+        memberEnds(i,:) = [str2double(tableData{i,3}),str2double(tableData{i,4})];
     end
-end
-Sx = zeros(size(uniqueJoints,1), 3);
-Sy = zeros(size(uniqueJoints,1), 3);
-Sx(1,1) = 1;
-Sy(1,2) = 1;
-Sy(index,3) = 1; 
-A =  [A [Sx;Sy]];
-
-mg = 30*9.81;
-L = zeros(2*size(uniqueJoints,1),1);
-L(13,1) = -mg;
-
-Tension = forceCalculations(A,L);
-function [] = display(T,N)
-    num= size(N,1);
-    for i = 1:num
-        fprintf("Member %s experiences %.2f \n",N(i),T(i));
-    end 
-    fprintf("Sx1 = %.2f \n,",T(num-2));
-    fprintf("Sy1 = %.2f \n",T(num-1));
-    fprintf("Sy2 = %.2f \n",T(num));
-end 
-function printResults(memberForces, reactionForces, memberLengths, load, cost)
-    % Header
-    fprintf('EK301, Section A1, Group X: Name1, Name2, Name3, Date\n');
-    fprintf('Load: %g oz\n\n', load);
-
-    % Member forces
-    fprintf('Member forces in oz:\n');
-    for i = 1:length(memberForces)
-        if memberForces(i) < 0
-            label = 'C';
-        else
-            label = 'T';
+    
+    [uniqueJoints,~,jointMap] = unique([joints;memberEnds], 'rows', 'stable');
+    %jointMap
+    %jointMap
+    %example on how to index map
+    %{
+        for i = 1:size(joints(:,1))
+        disp(joints(i,:))
+        disp(uniqueJoints(jointMap(i),:))
+    end  
+    %}
+    
+    [A,names,r] = dimToVector(joints,memberEnds,uniqueJoints);
+    index = 0;
+    big = uniqueJoints(1,1);
+    for i = 2:size(uniqueJoints,1)
+        if uniqueJoints(i,1) >= big
+            big = uniqueJoints(i,1);
+            index = i;
         end
-        fprintf('m%d: %.3f (%s)\n', i, abs(memberForces(i)), label);
     end
+    Sx = zeros(size(uniqueJoints,1), 3);
+    Sy = zeros(size(uniqueJoints,1), 3);
+    Sx(1,1) = 1;
+    Sy(1,2) = 1;
+    Sy(index,3) = 1; 
+    A =  [A [Sx;Sy]];
+    
+    L = zeros(2*size(uniqueJoints,1),1);
+    L(jointIndex,1) = -100*F;
+    
+    Tension = forceCalculations(A,L);
+    function [] = display(T,N)
+        num= size(N,1);
+        for i = 1:num
+            fprintf("Member %s experiences %.2f \n",N(i),T(i));
+        end 
+        fprintf("Sx1 = %.2f \n,",T(num-2));
+        fprintf("Sy1 = %.2f \n",T(num-1));
+        fprintf("Sy2 = %.2f \n",T(num));
+    end 
+end
 
-    % Reaction forces
-    fprintf('\nReaction forces in oz:\n');
-    reactionLabels = {'Sx1', 'Sy1', 'Sy2'};
-    for i = 1:length(reactionForces)
-        fprintf('%s: %.3f\n', reactionLabels{i}, reactionForces(i));
-    end
+% function printResults(memberForces, reactionForces, memberLengths, load, cost)
+%     % Header
+%     fprintf('EK301, Section A1, Group X: Name1, Name2, Name3, Date\n');
+%     fprintf('Load: %g oz\n\n', load);
+% 
+%     % Member forces
+%     fprintf('Member forces in oz:\n');
+%     for i = 1:length(memberForces)
+%         if memberForces(i) < 0
+%             label = 'C';
+%         else
+%             label = 'T';
+%         end
+%         fprintf('m%d: %.3f (%s)\n', i, abs(memberForces(i)), label);
+%     end
+% 
+%     % Reaction forces
+%     fprintf('\nReaction forces in oz:\n');
+%     reactionLabels = {'Sx1', 'Sy1', 'Sy2'};
+%     for i = 1:length(reactionForces)
+%         fprintf('%s: %.3f\n', reactionLabels{i}, reactionForces(i));
+%     end
+% 
+%     % Cost and ratio
+%     fprintf('\nCost of truss: $%.2f\n', cost);
+%     fprintf('Load/cost ratio: %.4f oz/$\n', load/cost);
+% 
+%     %Theoretical max load/cost ratio in oz/$: 0.0031
+% 
+% end 
 
-    % Cost and ratio
-    fprintf('\nCost of truss: $%.2f\n', cost);
-    fprintf('Load/cost ratio: %.4f oz/$\n', load/cost);
 
-    %Theoretical max load/cost ratio in oz/$: 0.0031
 
-end 
-
-display(Tension,names)
